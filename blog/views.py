6f65169090839e446394a749750840e80c4becb2
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-09-15 07:36:43 -0300

    aula 10

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def list_posts(request):
-    return render(request, 'posts-list.html')
+    return render(request, 'blog/posts-list.html')
 
 def detail_post(request, pk):
-    return render(request, 'post-detail.html')
+    return render(request, 'blog/post-detail.html')
