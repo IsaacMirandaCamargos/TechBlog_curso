7fdb563f60dc89610e340995a15c49bc3bf7bc90
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-09-18 20:33:14 -0300

    aula 17

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,13 @@ from blog.models import Category, Post, Tag
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+#admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_author_name', 'title', 'created_at')
+
+    def full_author_name(self, obj):
+        return f"{obj.author.first_name} {obj.author.last_name}"
+    full_author_name.short_description = "author"
