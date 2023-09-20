2591263989f1805881bbc93cb5e507406e8c22e0
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-09-19 21:53:25 -0300

    aula 23

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,11 +1,14 @@
-from blog.models import Category, Tag
-from blog.utils import format_tag
+from blog.models import Category, Tag, Post
 
 def categories_and_tags(request):
     context = dict()
 
     categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    for category in categories:
+        count = Post.objects.filter(category=category.id).count()
+        category.count = count
 
     context['categories'] = categories
     context['tags'] = tags
