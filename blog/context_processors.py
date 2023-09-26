1d1c7f735020ac13467803c9a4e1483f3bfafa19
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-09-25 21:58:13 -0300

    aula 29

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -10,6 +10,9 @@ def categories_and_tags(request):
         count = Post.objects.filter(category=category.id).count()
         category.count = count
 
+    last_posts = Post.objects.order_by('-created_at')[:3]
+
+    context['last_posts'] = last_posts
     context['categories'] = categories
     context['tags'] = tags
 
