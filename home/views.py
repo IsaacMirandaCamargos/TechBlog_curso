cb9599d843c6866af269a2945120a5a23491ac31
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-10-01 14:02:24 -0300

    aula 33

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from blog.models import MainFeaturedPost
+from blog.utils import format_body
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html')
+    context = dict()
+
+    main_featured_posts = MainFeaturedPost.objects.filter(active=True)[:3]
+    for mfpost in main_featured_posts:
+        mfpost.post.mini_description = format_body(mfpost.post.body, 20)
+
+    context['main_featured_posts'] = main_featured_posts
+
+    return render(request, 'blog/index.html', context=context)
