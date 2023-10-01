56750ff4c73afa30291063fc61470109727d0521
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-10-01 11:11:15 -0300

    aula 31

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,5 +1,6 @@
 from blog.models import Category, Tag, Post
 from blog.utils import format_body
+from hitcount.models import HitCount
 
 def categories_and_tags(request):
     context = dict()
@@ -15,6 +16,11 @@ def categories_and_tags(request):
     for lpost in last_posts:
         lpost.mini_description = format_body(lpost.body)
 
+    hits = HitCount.objects.all()[:3]
+    object_pks = [hit.object_pk for hit in hits]
+    most_viewed = [Post.objects.get(pk=pk) for pk in object_pks]
+    
+    context['most_viewed'] = most_viewed
     context['last_posts'] = last_posts
     context['categories'] = categories
     context['tags'] = tags
