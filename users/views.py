525d7d328f96bf6f7001348a011984681b307898
IsaacMirandaCamargos <isaacmirandacamargos@gmail.com>
2023-10-14 17:48:47 -0300

    aula 45

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-
+from django.contrib import messages
 from users.forms import NewsletterSignupForm
 
 # Create your views here.
@@ -9,6 +9,10 @@ def newsletter_signup(request):
         form = NewsletterSignupForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Seu e-mail foi cadastrado com sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, "Falha no cadastro por e-mail invalido ou e-mail já cadastrado!")
             return redirect('home')
     else:
         return redirect('home')
